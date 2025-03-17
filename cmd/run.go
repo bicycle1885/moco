35545ea8179f13dcc47973a88cfa050490dc9c6c
Kenta Sato <bicycle1885@gmail.com>
2025-03-17 15:15:12 +0100

    set SilenceUsage true for the run command

--- cmd/run.go
+++ cmd/run.go
@@ -8,9 +8,10 @@ import (
 
 func init() {
 	runCmd := &cobra.Command{
-		Use:     "run [command]",
-		Aliases: []string{"r"},
-		Short:   "Run a command in an experiment directory with metadata tracking",
+		Use:          "run [command]",
+		Aliases:      []string{"r"},
+		SilenceUsage: true,
+		Short:        "Run a command in an experiment directory with metadata tracking",
 		Long: `Run a command with full reproducibility tracking.
 
 This command will:
